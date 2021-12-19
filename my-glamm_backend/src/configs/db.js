649002ce52f26d52ee5913c04c47d918681f7984
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2021-12-19 13:57:27 +0530

    Delete my-glamm_backend directory

--- my-glamm_backend/src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-  mongoose.connect("mongodb+srv://prajwal:prajwalrathod@cluster0.qparh.mongodb.net/myglamm");
-};
-
-module.exports = connect;
