e12290b30d9541aa830f654131755e0c13ade781
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2021-12-19 13:58:42 +0530

    With backend

---
+++ payment/payment.js
@@ -0,0 +1,24 @@
+let card = document.getElementById("card");
+let name = document.getElementById("name");
+let validity = document.getElementById("validity");
+var paymentbtm = document.getElementById("pay");
+paymentbtm.onclick = function () {
+  if (
+    card.value != null &&
+    card.value != "" &&
+    name.value != null &&
+    name.value != "" &&
+    validity.value != null &&
+    validity.value != ""
+  )
+    window.location.href = "/successful";
+};
+
+let total01 = JSON.parse(localStorage.getItem("p_amount"));
+let stotal = total01[0];
+console.log(stotal);
+
+var total = document.getElementById("total");
+total.innerHTML = null;
+var srng = stotal;
+total.append("Rs " + srng);
